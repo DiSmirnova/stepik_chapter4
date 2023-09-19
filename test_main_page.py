749e1cc11023a4147f6b368d083293330b118830
DiSmirnova <119680697+DiSmirnova@users.noreply.github.com>
2023-09-19 11:42:08 +0300

    Сompleted task chapter 4 lesson 2 step 8

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-
-def go_to_login_page(browser):
-    link = browser.find_element(By.CSS_SELECTOR,"#login_link")
-    link.click()
 
 def test_guest_can_go_to_login_page(browser):
    page = MainPage(browser, link)
@@ -18,3 +15,18 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_login_url(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_register_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
