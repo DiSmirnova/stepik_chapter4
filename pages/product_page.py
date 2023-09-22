b2b75bde94fef5a9172340c2194a4ec55b517443
DiSmirnova <119680697+DiSmirnova@users.noreply.github.com>
2023-09-22 15:09:18 +0300

    Update product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,8 +1,22 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class ProductPage(BasePage):
     def add_product_to_basket_with_alert_check(self):
         basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         basket.click()
         BasePage.solve_quiz_and_get_code(self)
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_succes_massage_dissapeard(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not dissapeard, but should be"
+
+    def add_product_to_basket(self):
+        basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket.click()
